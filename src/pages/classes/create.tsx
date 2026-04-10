e13d9287e035677609af781f1d6b3d22d689a37b
RawdonNoronha <rawdonnoronha11@gmail.com>
2026-04-11 00:01:42 +0530

    Implemented upload widget

---
+++ src/pages/classes/create.tsx
@@ -0,0 +1,319 @@
+import { Breadcrumb } from "@/components/refine-ui/layout/breadcrumb";
+import { CreateView } from "@/components/refine-ui/views/create-view";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Separator } from "@/components/ui/separator";
+import { useBack } from "@refinedev/core";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { classSchema } from "@/lib/schema";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import UploadWidget from "@/components/upload-widget";
+
+const teachers = [
+  { id: 1, name: "John Doe" },
+  { id: 2, name: "Jane Smith" },
+];
+
+const subjects = [
+  { ID: 1, name: "Mathematics", code: "MATH" },
+  { ID: 2, name: "Biology", code: "BIO" },
+];
+
+const ClassesCreate = () => {
+  const back = useBack();
+
+  const form = useForm<z.infer<typeof classSchema>>({
+    resolver: zodResolver(classSchema),
+    defaultValues: {
+      name: "",
+      description: "",
+      subjectId: undefined,
+      teacherId: "",
+      capacity: 0,
+      status: "active",
+      bannerUrl: "",
+      bannerCldPubId: "",
+    },
+  });
+
+  const {
+    handleSubmit,
+    formState: { isSubmitting, errors },
+    control,
+  } = form;
+
+  const onSubmit = (_values: z.infer<typeof classSchema>) => {
+    try {
+    } catch (e) {
+      console.log("Error creating new classes", e);
+    }
+  };
+
+  const bannerPublicId = form.watch("bannerCldPubId");
+
+  const setBannerImage = (field: any, file: any) => {
+    if (file) {
+      field.onChange(file.url);
+      form.setValue("bannerCldPubId", file.publicId, {
+        shouldValidate: true, //validate the field when setting the value
+        shouldDirty: true, //mark the field as modified when setting the value
+      });
+    } else {
+      field.onChange("");
+      form.setValue("bannerCldPubId", "", {
+        shouldValidate: true,
+        shouldDirty: true,
+      });
+    }
+  };
+
+  return (
+    <CreateView className="class-view">
+      <Breadcrumb />
+
+      <h1 className="page-title">Create a Class</h1>
+
+      <div className="intro-row">
+        <p>Provide the required information below to add a class</p>
+        <Button onClick={back}>Go Back</Button>
+      </div>
+
+      <Separator />
+
+      <div className="my-4 flex items-center">
+        <Card className="class-form-card">
+          <CardHeader className="relative z-10">
+            <CardTitle className="text-2xl pb-0 font-bold">
+              Fill out the form
+            </CardTitle>
+          </CardHeader>
+
+          <Separator />
+
+          <CardContent className="mt-7">
+            <Form {...form}>
+              <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
+                <FormField
+                  control={control}
+                  name="bannerUrl"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>
+                        Banner Image<span className="text-orange-600">*</span>
+                      </FormLabel>
+                      <FormControl>
+                        <UploadWidget
+                          value={
+                            field.value
+                              ? {
+                                  url: field.value,
+                                  publicId: bannerPublicId ?? "",
+                                }
+                              : null
+                          }
+                          onChange={(file: any, field: any) =>
+                            setBannerImage(field, file)
+                          }
+                        />
+                      </FormControl>
+                      <FormMessage />
+                      {errors.bannerCldPubId && !errors.bannerUrl && (
+                        <p className="text-red-500 text-sm">
+                          {errors.bannerCldPubId.message?.toString()}
+                        </p>
+                      )}
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>
+                        Class Name<span className="text-orange-600">*</span>
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Introduction to Biology - Section A"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <div className="grid sm:grid-cols-2 gap-4">
+                  <FormField
+                    control={control}
+                    name="subjectId"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>
+                          Subject<span className={"text-orange-600"}>*</span>
+                        </FormLabel>
+                        <FormControl>
+                          <Select
+                            onValueChange={(value) =>
+                              field.onChange(Number(value))
+                            }
+                            value={field?.value?.toString()}
+                          >
+                            <SelectTrigger
+                              className={"w-full"}
+                              aria-invalid={!!errors.subjectId}
+                            >
+                              <SelectValue placeholder="Select a subject" />
+                            </SelectTrigger>
+                            <SelectContent>
+                              {subjects.map((subject) => (
+                                <SelectItem
+                                  key={subject.ID}
+                                  value={subject.ID.toString()}
+                                >
+                                  {subject.name + " (" + subject.code + ")"}
+                                </SelectItem>
+                              ))}
+                            </SelectContent>
+                          </Select>
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={control}
+                    name="teacherId"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>
+                          Teacher<span className="text-orange-600">*</span>
+                        </FormLabel>
+                        <FormControl>
+                          <Select
+                            onValueChange={(value) => field.onChange(value)}
+                            value={field?.value?.toString()}
+                          >
+                            <SelectTrigger
+                              className="w-full"
+                              aria-invalid={!!errors.teacherId}
+                            >
+                              <SelectValue placeholder="Select a teacher" />
+                            </SelectTrigger>
+                            <SelectContent>
+                              {teachers.map((teacher) => (
+                                <SelectItem
+                                  key={teacher.id}
+                                  value={teacher.id.toString()}
+                                >
+                                  {teacher.name}
+                                </SelectItem>
+                              ))}
+                            </SelectContent>
+                          </Select>
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+
+                <div className="grid sm:grid-cols-2 gap-4">
+                  <FormField
+                    control={control}
+                    name="capacity"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Capacity</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="number"
+                            placeholder="Enter capacity"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={control}
+                    name="status"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>
+                          Status<span className="text-orange-600">*</span>
+                        </FormLabel>
+                        <FormControl>
+                          <Select
+                            onValueChange={field.onChange}
+                            value={field.value}
+                          >
+                            <SelectTrigger
+                              className="w-full"
+                              aria-invalid={!!errors.status}
+                            >
+                              <SelectValue placeholder="Select status" />
+                            </SelectTrigger>
+                            <SelectContent>
+                              <SelectItem value="active">Active</SelectItem>
+                              <SelectItem value="inactive">Inactive</SelectItem>
+                            </SelectContent>
+                          </Select>
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+
+                <FormField
+                  control={control}
+                  name="description"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Description</FormLabel>
+                      <FormControl>
+                        <Textarea
+                          placeholder="Enter description"
+                          className="min-h-[100px]"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <Button type="submit">Submit</Button>
+              </form>
+            </Form>
+          </CardContent>
+        </Card>
+      </div>
+    </CreateView>
+  );
+};
+
+export default ClassesCreate;
