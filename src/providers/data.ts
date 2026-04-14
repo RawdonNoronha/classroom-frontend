4be6834735244fa4cb7fd7b75287553379c43f28
RawdonNoronha <rawdonnoronha11@gmail.com>
2026-04-14 11:45:34 +0530

    Add toast for rate limiting

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,8 +1,27 @@
 import { BACKEND_BASE_URL } from "@/constants";
 import { ListResponse } from "@/types";
+import { HttpError } from "@refinedev/core";
 import { createDataProvider, CreateDataProviderOptions } from "@refinedev/rest";
 
-if(!BACKEND_BASE_URL) throw new Error("BACKEND_BASE_URL is not defined in environment variables");
+if (!BACKEND_BASE_URL) throw new Error("BACKEND_BASE_URL is not defined in environment variables");
+
+const buildHttpErrors = async (response: Response): Promise<HttpError> => {
+  let message = 'Request failed';
+
+  try {
+    const payload = (await response.json()) as { message?: string }
+
+    if (payload?.message) message = payload.message;
+  }
+  catch {
+    //Ignore Errors
+  }
+
+  return {
+    message,
+    statusCode: response.status
+  }
+}
 
 const options: CreateDataProviderOptions = {
   getList: {
@@ -28,12 +47,16 @@ const options: CreateDataProviderOptions = {
     },
 
     mapResponse: async (response) => {
+      if (!response.ok) throw await buildHttpErrors(response);
+
       const payload: ListResponse = await response.clone().json();
 
       return payload.data ?? [];
     },
 
     getTotalCount: async (response) => {
+      if (!response.ok) throw await buildHttpErrors(response);
+
       const payload: ListResponse = await response.clone().json();
 
       return payload.pagination?.total ?? payload.data?.length ?? 0;
